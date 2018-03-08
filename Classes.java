8f78b74f7411f1152187ce0a88bbcf309386b68f
Naresh Gourishetty <naresh.gourishetty@kroger.com>
2018-03-07 22:38:18 -0500

    first commit

---
+++ Classes.java
@@ -0,0 +1,18 @@
+There are  types of classes:-
+	1. Normal classes
+	2. Abstract classes
+	3. Parent classes
+	4. Child classes
+	5. Java Bean classes
+	6. Adapter classes
+	7. Public classes
+	8. Default classes
+	9. Tighly Encapsuled classes
+	10. Implementation classes
+	11. Final classes
+	12. Immutable classes
+	13. Mutable classes
+	14. Strictfp classes
+			All the floating point calculations follow IEEE750 standards (refer to main method video)
+	15. Singleton classes
+			Allows to create only one object
